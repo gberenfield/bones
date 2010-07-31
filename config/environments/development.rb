7e4ac82bd1e78f42502ccfc582f972107285c62c
GB <gberenfield@berenfield.com>
2010-07-30 20:37:25 -0400

    * handle deprecation messages

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,4 +19,6 @@ Bones::Application.configure do
   
   # Devise requirement
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  
+  config.active_support.deprecation = :log
 end
